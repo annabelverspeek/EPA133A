b0bfc7bcfcb773a5b49c6fba4bd69631afd7ee7d
annasimulation <j.s.paardekooper@student.tudelft.nl>
2024-03-06 14:43:36 +0100

    transform data final! and added comments

--- Assignment2/EPA133a-Lab/EPA133a-G12-A2/model/Transform_data_N1.py
+++ Assignment2/EPA133a-Lab/EPA133a-G12-A2/model/Transform_data_N1.py
@@ -1,18 +1,15 @@
+# importing the needed libraries
 import pandas as pd
 import numpy as np
 
+# the cleaned bridges dataset is imported
 excel_file_bridge = 'BMMS_overview2.xlsx'
 df_bridge = pd.read_excel(excel_file_bridge)
+# only the columns in the bridge dataset are imported that align with the demofile. As extra, the condition of the bridges is added. The column condition is also added in the run methods to make sure this column is taken along.
 df_bridge = df_bridge[['road','name','lat','lon','length', 'condition','chainage','type']]
 
-# import the data from _roads3.csv
-# excel_file_road = '_roads3_2.csv'
-# df_roads = pd.read_csv(excel_file_road)
-# df_roads = df_roads[['road','name','lat','lon']]
-
-# Filter data for road 'N1'
+# Filter data for road 'N1'. Change to other road if you want to research other roads
 df_bridge = df_bridge[df_bridge['road'] == 'N1']
-# df_roads = df_roads[df_roads['road'] == 'N1']
 
 #removing double values for the bridges. Same latitudes and longitudes measured twice. Values with shortest length remain.
 df_bridge = df_bridge.sort_values('length')
@@ -21,7 +18,7 @@ df_bridge = df_bridge[['road','name','lat','lon','length', 'condition','chainage
 df_bridge = df_bridge.sort_values('chainage')
 
 #print(df_bridge)
-
+#an empty list is created for data.
 data = []
 
 # Initializing the variables
@@ -31,10 +28,8 @@ link_chainage = 0
 bridge_counter = 1  # Initialize bridge counter
 link_chainage = 0    # Initialize link chainage
 
-#df_roads.loc[0, "model_type"]="source"
-#df_roads.loc[len(df_roads)-1, "model_type"] = "sink"
 
-# append source. As source the beginning of the road in Chittagong has been defined with specified latitudes and longitudes:
+# append source. As source the beginning of the road in Dhaka has been defined with specified latitudes and longitudes:
 data.append({'road': 'N1', 'id': id_counter, 'model_type': 'source', 'name': 'source', 'lat': 23.7060278, 'lon': 90.443333, 'length': 0, 'condition': np.nan})
 id_counter += 1 #Otherwise similar id for the source and the first bridge!
 
@@ -51,26 +46,27 @@ for index, row in df_bridge.iterrows():
     # we do the chainage times 1000 to go from meters to kilometers
     chainage = (float(chainage_str.replace(',', '.')))*1000  # Replace commas and convert to float
 
-    # Append link
-
+    # Append link.
     if index > 0:
         link_length = chainage - link_chainage  # Calculate the length of the link
         data.append(
-            {'road': road, 'id': id_counter, 'model_type': 'link', 'name': f'link {index}', 'lat': link_chainage,
-             'lon': link_chainage, 'length': link_length, 'condition': np.nan})
+            {'road': road, 'id': id_counter, 'model_type': 'link', 'name': f'link {index}', 'lat': lat,
+             'lon': lon, 'length': link_length, 'condition': np.nan})
         id_counter += 1
         link_chainage = chainage  # Update link chainage after calculating link_length
 
-    # Append bridge
-    if row['type'] == 'PC Girder Bridge' or row['type'] == 'Box Culvert' or row['type'] == 'PC Box' or row['type'] == 'RCC Girder Bridge' or row['type'] == 'Slab Culvert' or row['type'] == 'Steel Beam & RCC Slab' or row['type'] == 'Arch Masonry' or row['type'] == 'RCC Bridge' or row['type'] == 'Truss With Timber Deck':  # Check if it's a bridge
+    # Append bridge. All types of bridges are taken along
+    if row['type'] == 'PC Girder Bridge' or row['type'] == 'Box Culvert' or row['type'] == 'PC Box' or row['type'] == 'RCC Girder Bridge' or row['type'] == 'Slab Culvert' or row['type'] == 'Steel Beam & RCC Slab' or row['type'] == 'Arch Masonry' or row['type'] == 'RCC Bridge'  or row['type'] == 'Baily with Steel Deck' or row['type'] == 'Truss with Steel Deck' or row['type'] == 'Truss with RCC Slab' or row['type'] == 'Baily with Timber Deck' or row['type'] == 'Pipe Culvert':  # Check if it's a bridge
         bridge_name = f"bridge {bridge_counter}"  # Use generic name for bridge
         data.append({'road': road, 'id': id_counter, 'model_type': 'bridge', 'name': bridge_name, 'lat': lat, 'lon': lon, 'length': length, 'condition': condition})
         id_counter += 1
         bridge_counter += 1  # Increment bridge counter
 
+    # if the latitude of the bridges pass the latitude of the sink, the adding of bridges to data is stopped.
     if row['lat'] < 22.3314716:
         break
 
+#append sink to data
 data.append({'road': 'N1', 'id': id_counter, 'model_type': 'sink', 'name': 'sink', 'lat': 22.3314716, 'lon': 91.8515556,
              'length': 0, 'condition': np.nan})
 id_counter += 1
@@ -80,12 +76,7 @@ new_df_bridge = pd.DataFrame(data)
 #print(new_df_bridge.head(10))
 
 
-# # Write the transformed data into a new CSV file
-# csv_file_path = 'transformed_data.csv'
-# new_df_bridge.to_csv(csv_file_path, index=False)
-#
-# print(f"CSV file '{csv_file_path}' has been created successfully.")
-
+# Write the transformed data into a new CSV file
 csv_file_path_N1 = 'transformed_data_N1.csv'
 new_df_bridge.to_csv(csv_file_path_N1, index=False)
 print(f"CSV file '{csv_file_path_N1}' has been created successfully.")
