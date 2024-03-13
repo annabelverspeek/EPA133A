56289b5d890371524a50778d8d3c179ad23a2b77
annasimulation <j.s.paardekooper@student.tudelft.nl>
2024-03-13 15:53:09 +0100

    transform python file to add sources and sinks

--- Assignment3.0/EPA1352-G12-A3/EPA1352-G12-A3/model/transform_N1_and_N2.py
+++ Assignment3.0/EPA1352-G12-A3/EPA1352-G12-A3/model/transform_N1_and_N2.py
@@ -1,10 +1,74 @@
 import numpy as np
 import pandas as pd
 
-#import the data of the overview
+# Import the data of the overview
 Overview_file = '_overview.xlsx'
 Overview = pd.read_excel(Overview_file)
 
-# Filteren welke wegen langer zijn dan 25 km
+# Filter which roads are longer than 25 km
 filtered_df = Overview[Overview['length'] >= 25]
-print(filtered_df.info)
+
+# Read the tsv file of roads
+df_roads = pd.read_csv("_roads.tsv", delimiter='\t')
+
+df_sourcesinks = pd.DataFrame(columns=['road', 'model_type', 'name', 'lat', 'lon'])
+
+data = []
+
+counter = 1
+
+# Iterate over each unique road
+for road in df_roads['road'].unique():
+    road_data = df_roads[df_roads['road'] == road]
+    # Extract the first LRP from the first row
+    first_lrp = road_data.iloc[0]
+    data.append({
+        'road': first_lrp['road'],
+        'model_type': 'sourcesink',
+        'name': f'SoSi{counter}',
+        'lat': first_lrp['lat1'],
+        'lon': first_lrp['lon1']
+    })
+    counter += 1
+
+    # Iterate over each unique road
+    for road in df_roads['road'].unique():
+        road_data = df_roads[df_roads['road'] == road]
+        # Extract the first LRP from the first row
+        first_lrp = road_data.iloc[0]
+        data.append({
+            'road': first_lrp['road'],
+            'model_type': 'sourcesink',
+            'name': f'SoSi{counter}',
+            'lat': first_lrp['lat1'],
+            'lon': first_lrp['lon1']
+        })
+        counter += 1
+
+        # Extract the last LRP from the last row
+        last_lrp = None
+        last_lrp_lat = None
+        last_lrp_lon = None
+
+        # Iterate over rows in reverse order to find the last LRP with a non-null value
+        for idx, row in road_data.iloc[1:][::-1].iterrows():  # Skip the first row (column names)
+            # Find the last non-null value in the row
+            last_valid_index = row.last_valid_index()
+            if last_valid_index is not None:
+                last_valid_index = int(last_valid_index)  # Convert to integer
+                last_lrp_lat = row[last_valid_index - 1]  # Latitude is the value before the last valid index
+                last_lrp_lon = row[last_valid_index]      # Longitude is the last valid value
+                break
+
+        data.append({
+            'road': road,
+            'model_type': 'sourcesink',
+            'name': f'SoSi{counter}',
+            'lat': last_lrp_lat,
+            'lon': last_lrp_lon
+        })
+        counter += 1
+
+# Create DataFrame from collected data
+df_sourcesinks = pd.DataFrame(data)
+#print(df_sourcesinks.head())
