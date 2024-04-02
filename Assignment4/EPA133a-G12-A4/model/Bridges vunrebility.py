4ffeab8dc2f7cecfba63e717a0e1a1c86d33a3cf
annabelverspeek <annabel.verspeek@live.nl>
2024-04-02 12:24:45 +0200

    bridges vulnerability

---
+++ Assignment4/EPA133a-G12-A4/model/Bridges vunrebility.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+file1 = '../model/bridges.csv'
+bridges = pd.read_csv(file)
+
+file2 = '../model/vulnerability_water_distance.xls'
+vulnerabilities = pd.read_excel(file2)
+
+
+for index, row in bridges.iterrows():
+    division = row['division']
+
+    if row['division'] in vulnerabilities:
+        score = vulnerabilities['vulnerability']
+        if score > 0.4 and score <= 0.45:
+            bridges.at[index, 'condition_water'] = 'A'
+        elif score > 0.45 and score <= 0.5:
+            bridges.at[index, 'condition_water'] = 'B'
+        elif score > 0.5 and score <= 0.55:
+            bridges.at[index, 'condition_water'] = 'C'
+        elif score > 0.55 and score <= 0.6:
+            bridges.at[index, 'condition_water'] = 'D'
+
+bridges.to_csv('../model/bridges.csv', index=False)
+
+
