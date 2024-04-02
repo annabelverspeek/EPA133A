884c983c98c0e0088ba609bad01b08f6144c5089
annabelverspeek <annabel.verspeek@live.nl>
2024-04-02 14:09:26 +0200

    bridges vulnerability not foutmelding

--- Assignment4/EPA133a-G12-A4/model/Bridges vunrebility.py
+++ Assignment4/EPA133a-G12-A4/model/Bridges vunrebility.py
@@ -6,53 +6,28 @@ bridges = pd.read_excel(file1, engine = 'openpyxl')
 file2 = '../data/Vulnerability_water_distance.xlsx'
 vulnerabilities = pd.read_excel(file2, engine = 'openpyxl')
 
+bridges['condition_water'] = None
 
 for index, row in bridges.iterrows():
     division = row['division']
 
-    if row['division'] in vulnerabilities:
-        score = vulnerabilities['Vulnerability']
+    # Filter vulnerabilities DataFrame based on current division
+    division_vulnerability = vulnerabilities[vulnerabilities['District'] == division]
+    print(division_vulnerability)
+
+    # Check if division exists in vulnerabilities DataFrame
+    if not division_vulnerability.empty:
+        score = division_vulnerability['Vulnerability']
+        print(score)
         if score > 0.4 and score <= 0.45:
-            bridges.at[index, 'condition_water'] = 'A'
+            bridges.loc[index, 'condition_water'] = 'A'
         elif score > 0.45 and score <= 0.5:
-            bridges.at[index, 'condition_water'] = 'B'
+            bridges.loc[index, 'condition_water'] = 'B'
         elif score > 0.5 and score <= 0.55:
-            bridges.at[index, 'condition_water'] = 'C'
+            bridges.loc[index, 'condition_water'] = 'C'
         elif score > 0.55 and score <= 0.6:
-            bridges.at[index, 'condition_water'] = 'D'
-    else: print(row['division'])
+            bridges.loc[index, 'condition_water'] = 'D'
+    # else: print(row['division'])
 
 bridges.to_excel('../data/processed/BMMS_overview.xlsx', index=False)
 
-
-
-
-# import pandas as pd
-#
-# file1 = '../data/processed/BMMS_overview.xlsx'
-# bridges = pd.read_excel(file1)
-#
-# file2 = '../data/Vulnerability_water_distance.xlsx'
-# vulnerabilities = pd.read_excel(file2)
-#
-# for index, row in bridges.iterrows():
-#     division = row['division']
-#
-#     # Check if division exists in vulnerabilities DataFrame
-#     if division in vulnerabilities['division'].values:
-#         # Get vulnerability score for the current division
-#         score = vulnerabilities.loc[vulnerabilities['division'] == division, 'vulnerability'].values[0]
-#
-#         # Assign condition rating based on the vulnerability score
-#         if score > 0.4 and score <= 0.45:
-#             bridges.at[index, 'condition_water'] = 'A'
-#         elif score > 0.45 and score <= 0.5:
-#             bridges.at[index, 'condition_water'] = 'B'
-#         elif score > 0.5 and score <= 0.55:
-#             bridges.at[index, 'condition_water'] = 'C'
-#         elif score > 0.55 and score <= 0.6:
-#             bridges.at[index, 'condition_water'] = 'D'
-#     else:
-#         print(row['division'])
-#
-# bridges.to_excel('../data/processed/BMMS_overview.xlsx', index=False)
