75ddd55a7adbc72dcafd10852ac3da3c472b369e
luisaxh <l.j.ximenezbruidegom@student.tudelft.nl>
2024-04-03 12:34:26 +0200

    vulnerability on bridge conditions and flood risk

--- Assignment4/EPA133a-G12-A4/model/Bridge_conditions_added.py
+++ Assignment4/EPA133a-G12-A4/model/Bridge_conditions_added.py
@@ -2,8 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
+###############################
+### Prepare both data files ###
+###############################
+
 # Load BMMS overview into a pandas DataFrame
-bmms_df = pd.read_excel('BMMS_overview.xlsx')
+# This BMMS overview file now also contains the extra column flood_risk
+path_bmms = '../data/processed/BMMS_overview.xlsx'
+bmms_df = pd.read_excel(path_bmms)
 # Load merged traffic data into a pandas DataFrame
 traffic_df = pd.read_csv('latlonloads.csv')
 
@@ -20,8 +26,12 @@ bmms_df['chainage'] = bmms_df['chainage'].astype(float)
 traffic_df['Chainage Start'] = traffic_df['Chainage Start'].astype(float)
 traffic_df['Chainage End'] = traffic_df['Chainage End'].astype(float)
 
+#######################################
+### Prepare bridge condition counts ###
+#######################################
+
 # Create a dictionary where each key will be a road segment name,
-# and the value will be another dictionary holding the counts for each condition.
+# and the value will be another dictionary holding the counts for each bridge condition A,B,C or D.
 road_segment_conditions = {}
 
 # Iterate through bridges in the BMMS DataFrame
@@ -39,7 +49,7 @@ for index, bridge_row in bmms_df.iterrows():
     for i, segment_row in matching_segments.iterrows():
         segment_name = segment_row['name']
 
-        # Initialize a new segment in the dictionary if it does not exist
+        # Initialize a new segment in the dictionary if it does not exist, set the initial value on zero.
         if segment_name not in road_segment_conditions:
             road_segment_conditions[segment_name] = {
                 'Condition_A_Count': 0,
@@ -63,21 +73,76 @@ road_segment_conditions_df = pd.DataFrame.from_dict(road_segment_conditions, ori
 road_segment_conditions_df.reset_index(inplace=True)
 road_segment_conditions_df.rename(columns={'index': 'name'}, inplace=True)
 
-# Merge the counts DataFrame with the traffic DataFrame on Road_Segment_ID
-final_df = pd.merge(traffic_df, road_segment_conditions_df, on='name', how='left')
+##############################################
+### Prepare bridge chancec on flood counts ###
+##############################################
 
-# Add a new column to merged_df for the weighted sum
+# Create a dictionary where each key will be a road segment name,
+# and the value will be another dictionary holding the counts for each flood risk A,B,C or D.
+road_segment_flood_risk = {}
+
+# Iterate through bridges in the BMMS DataFrame
+for index, bridge_row in bmms_df.iterrows():
+    bridge_road = bridge_row['road']
+    bridge_flood_risk = bridge_row['flood_risk']
+    bridge_chainage = bridge_row['chainage']
+
+    # Find the road segment where the bridge chainage falls within its range
+    matching_segments = traffic_df[(traffic_df['road'] == bridge_road) &
+                                   (traffic_df['Chainage Start'] <= bridge_chainage) &
+                                   (traffic_df['Chainage End'] >= bridge_chainage)]
+
+    # Increment the count of the bridge condition for each matching road segment
+    for i, segment_row in matching_segments.iterrows():
+        segment_name = segment_row['name']
+
+        # Initialize a new segment in the dictionary if it does not exist, set the initial on zero.
+        if segment_name not in road_segment_flood_risk:
+            road_segment_flood_risk[segment_name] = {
+                'Flood_Risk_A_Count': 0,
+                'Flood_Risk_B_Count': 0,
+                'Flood_Risk_C_Count': 0,
+                'Flood_Risk_D_Count': 0
+            }
+
+        # Increment the counter for the bridge's condition
+        if bridge_flood_risk == 'A':
+            road_segment_flood_risk[segment_name]['Flood_Risk_A_Count'] += 1
+        elif bridge_flood_risk == 'B':
+            road_segment_flood_risk[segment_name]['Flood_Risk_B_Count'] += 1
+        elif bridge_flood_risk == 'C':
+            road_segment_flood_risk[segment_name]['Flood_Risk_C_Count'] += 1
+        elif bridge_flood_risk == 'D':
+            road_segment_flood_risk[segment_name]['Flood_Risk_D_Count'] += 1
+
+# Now convert this nested dictionary into a DataFrame
+road_segment_flood_risk_df = pd.DataFrame.from_dict(road_segment_flood_risk, orient='index')
+road_segment_flood_risk_df.reset_index(inplace=True)
+road_segment_flood_risk_df.rename(columns={'index': 'name'}, inplace=True)
+
+# Merge the counts DataFrame with the traffic DataFrame on name, both for the conditions counts
+# as the flood risks counts.
+final_df_1 = pd.merge(traffic_df, road_segment_conditions_df, on='name', how='left')
+final_df = pd.merge(final_df_1, road_segment_flood_risk_df, on='name', how='left')
+
+# Add a new column to final_df for the weighted sum
+# The weighted sum is the count of A,B,C and D conditions and flood risks times a weight.
+# Condition A and flood risk A have the lowest weight, these are the least vulnerable.
+# Condition D and flood risk D have the highest weight, these are the most vulnerable.
 final_df['Weighted_Sum'] = (final_df['Condition_A_Count'] * 1) + \
                              (final_df['Condition_B_Count'] * 2) + \
                              (final_df['Condition_C_Count'] * 3) + \
-                             (final_df['Condition_D_Count'] * 4)
+                             (final_df['Condition_D_Count'] * 4) + \
+                             (final_df['Flood_Risk_A_Count'] * 1) + \
+                             (final_df['Flood_Risk_B_Count'] * 2) + \
+                             (final_df['Flood_Risk_C_Count'] * 3) + \
+                             (final_df['Flood_Risk_D_Count'] * 4)
 
-final_df.column()
 
 # Save the result to a new CSV file
 final_df.to_csv('bridge_condition_counts.csv', index=False)
 
-# Plot the 10 most critical road segments according to the bridge condition.
+# Plot the 10 most critical road segments according to the weighted sum.
 
 # Sort the DataFrame based on 'Weighted_Sum' column in descending order
 sorted_merged_df = final_df.sort_values('Weighted_Sum', ascending=False)
@@ -97,10 +162,10 @@ weighted_values_10 = top_10_df['Weighted_Sum']
 
 # Plot the bar chart for the top 10 roads.
 plt.figure(figsize=(10, 6))
-plt.barh(road_names_10, weighted_values_10, color='skyblue')
-plt.xlabel('Vulnerability of Roads based on Bridge Conditions')
+plt.barh(road_names_10, weighted_values_10, color='red')
+plt.xlabel('Vulnerability of Roads')
 plt.ylabel('Road Names')
-plt.title(f'Top 10 Roads by Condition of Bridges')
+plt.title(f'Top 10 Roads by Vulnerability')
 plt.gca().invert_yaxis()  # Invert y-axis to display the highest-ranking road at the top
 plt.show()
 
@@ -113,14 +178,14 @@ for idx, bar in enumerate(bars):
     if road_names_100.iloc[idx] in top_10_df['name'].values:
         bar.set_color('red')
 
-plt.xlabel('Vulnerability of Roads based on Bridge Conditions')
+plt.xlabel('Vulnerability of Roads')
 plt.ylabel('')
-plt.title('Top 100 Roads by Condition of Bridges')
+plt.title('Top 100 Roads by Vulnerability')
 plt.gca().invert_yaxis()  # Invert y-axis to display the highest-ranking road at the top
 plt.yticks([])
 plt.show()
 
-# Plot the vulnerability based on the condition of bridges spatially.
+# Plot the vulnerability based on the condition of bridges and the flood risk of bridges spatially.
 
 # Convert latitude and longitude columns to numeric types, handling errors
 final_df['Latitude Decimal'] = pd.to_numeric(final_df['Latitude Decimal'], errors='coerce')
@@ -147,8 +212,8 @@ ax.set_xlim(minx, maxx)
 ax.set_ylim(miny, maxy)
 
 # Plot the GeoDataFrame with EVV values
-gdf.plot(ax=ax, marker='o', markersize=5, column='EVV', cmap='coolwarm', legend=True, vmin=0, vmax=55000)
+gdf.plot(ax=ax, marker='o', markersize=5, column='Weighted_Sum', cmap='coolwarm', legend=True, vmin=0, vmax=800)
 
 # Add title and show plot
-plt.title('Economic Value of Roads')
+plt.title('Vulnerability of Roads')
 plt.show()
